3e8b395f40968096e14e7aaa930acf478c386ca8
Marian Fernando <mdfernando3@myseneca.ca>
2025-08-06 22:37:39 -0400

    Added lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: mdfernando3
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
@@ -20,7 +20,18 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
-    return sum
+    while sum.second >= 60:
+        sum.second -= 60
+        sum.minute += 1
+
+    while sum.minute >= 60:
+        sum.minute -= 60
+        sum.hour += 1    
+
+    while sum.hour >=24:
+        sum.hour -= 24
+    return sum        
+
 
 def valid_time(t):
     """check for the validity of the time object attributes:
